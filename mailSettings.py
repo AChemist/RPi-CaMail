0390e9b48ab2504a311f85a14e9862465d00c334
AChemist <sebastian.malchow@posteo.de>
2016-01-01 22:55:50 +0100

    Initiall files added

---
+++ mailSettings.py
@@ -0,0 +1,8 @@
+smtp_host = ""
+smtp_port = 25
+
+smtp_username = ""
+smtp_password = ""
+
+smtp_sender = ""
+smtp_receiver = ""
---
+++ main.py
@@ -0,0 +1,36 @@
+# Code to sense motion using an passive infra red (PIR) sensor on a Raspberry PI.
+# After detection of motion a photo is recorded using the PiCamera and send via Mail.
+
+# Librarys
+import time 
+import RPi.GPIO as GPIO
+import smptlib
+
+# Settings
+import mailSettings
+
+# Functions
+
+
+# 
+GPIO.setmode(GPIO.BCM)
+PIR_PIN = 7
+GPIO.setup(PIR_PIN, GPIO.IN)
+
+
+
+def MOTION(PIR_PIN):
+	print "Motion Detected!"
+
+print "PIR Module Test (CTRL+C to exit)"
+time.sleep(2)
+print "Ready"
+
+try:
+	GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
+	while 1:
+		time.sleep(100)
+
+except KeyboardInterrupt:
+	print  " -> Quit"
+	GPIO.cleanup()
